f6d4e00cda53139065df4ed1033d2e0ed8fd8b82
unknown <dl.kokhtashvili@gmail.com>
2025-04-06 03:22:24 +0400

    Header add logo, used Nav in Header with manu links and structured footer  with semantic tags

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,12 @@
 import Nav from "./Nav";
+import logo from "../assets/Logo.svg";
 
 const Header = () => {
 
     return (
 
       <header>
+        <img src={logo} alt="Logo" />
        <Nav />
       </header>
 
