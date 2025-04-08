6fabfc330ef4adcfa48bbe994dc20b811c96ed83
unknown <dl.kokhtashvili@gmail.com>
2025-04-08 18:23:11 +0400

    add button component

---
+++ src/components/Button/Button.js
@@ -0,0 +1,13 @@
+import styles from "./Button.module.css";
+
+const Button = () => {
+
+    return  (
+        <button className={styles.buttonContainer}>
+             Reserve a Table
+        </button>
+    )
+}
+
+
+export default Button;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,15 +1,25 @@
-import restauranfood from "../../assets/restauranfood.jpg"
+import restauranfood from "../../assets/restauranfood.jpg";
 import styles from "./Hero.module.css";
+import Button from "../Button/Button";
 
 const Hero = () => {
   return (
     <div className={styles.heroContainer}>
       <h1>Little Lemon</h1>
       <h2>Chicago</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-      <img src={restauranfood} alt="restauranfood"/>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+      <div className={styles.herroButton}>
+        <Button />
+      </div>
+      <div className={styles.heroImageContainer}>
+      <img src={restauranfood} alt="restauranfood" />
+      </div>
     </div>
   );
 };
+
 
 export default Hero;
